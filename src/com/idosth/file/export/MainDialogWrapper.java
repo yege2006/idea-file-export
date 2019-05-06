d2f54bd9b2148ebd87b0e284dfe8e7433c999a51
yege2006 <yegemail@163.com>
2019-05-06 11:48:36 +0800

    添加注释，改写lambda表达式

--- src/com/idosth/file/export/MainDialogWrapper.java
+++ src/com/idosth/file/export/MainDialogWrapper.java
@@ -21,24 +21,25 @@ public class MainDialogWrapper extends JDialog {
     private JPanel fileListPanel;
     private JRadioButton onlyFileRadio;
     private JRadioButton structureRadio;
-    private JLabel pathLabel;
     //用于接收选中的文件
     private AnActionEvent event;
-    private JBList fieldList;
+    private String exportModel;
 
-    public MainDialogWrapper(AnActionEvent event) {
+    MainDialogWrapper(AnActionEvent event) {
+        this.event = event;
+        this.setTitle("Export");
         //初始默认选中导出文件带目录结构
         structureRadio.setSelected(true);
         //设置内容Panel
         initListeners();
-        this.setTitle("Export");
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
-
-        initFileList(event);
     }
 
+    /**
+     * ok按钮确认导出点击事件
+     */
     private void onOK() {
         dispose();
     }
@@ -47,35 +48,26 @@ public class MainDialogWrapper extends JDialog {
         dispose();
     }
 
-    private void initListeners(){
+    private void initListeners() {
 
-        browseBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //添加文件选择器
-                FileChooserDescriptor descriptor = new FileChooserDescriptor(false,true,false,false,false,false);
-                descriptor.setShowFileSystemRoots(true);
-                descriptor.setHideIgnored(true);
-                VirtualFile virtualFile = FileChooser.chooseFile(descriptor,null,null);
-                if (virtualFile.exists()) {
-                    pathEditText.setText(virtualFile.getCanonicalPath());
-                }
+        browseBtn.addActionListener(e -> {
+            //添加文件选择器
+            FileChooserDescriptor descriptor = new FileChooserDescriptor(false, true, false, false, false, false);
+            descriptor.setShowFileSystemRoots(true);
+            descriptor.setHideIgnored(true);
+            VirtualFile virtualFile = FileChooser.chooseFile(descriptor, null, null);
+            if (virtualFile != null && virtualFile.exists()) {
+                pathEditText.setText(virtualFile.getCanonicalPath());
             }
         });
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
+        //确定点击事件
+        buttonOK.addActionListener(e -> onOK());
 
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
+        //取消点击事件
+        buttonCancel.addActionListener(e -> onCancel());
 
-        // call onCancel() when cross is clicked
+        //关闭点击事件
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -84,16 +76,16 @@ public class MainDialogWrapper extends JDialog {
         });
 
         // 按下ESC关闭事件
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void initFileList(AnActionEvent event) {
+    /**
+     * 自定义Panel
+     */
+    private void createUIComponents() {
+        //获取选中的虚拟文件
         VirtualFile[] data = event.getData(DataKeys.VIRTUAL_FILE_ARRAY);
-        fieldList = new JBList(data);
+        JBList fieldList = new JBList(data);
         fieldList.setEmptyText("No File Selected!");
         ToolbarDecorator decorator = ToolbarDecorator.createDecorator(fieldList);
         fileListPanel = decorator.createPanel();
